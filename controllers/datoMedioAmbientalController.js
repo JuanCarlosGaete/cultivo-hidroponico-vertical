5834c4378476ab8496b34e4041c393091fbf3245
unknown <j.c.gaete.rojas@gmail.com>
2022-07-18 12:34:54 -0400

    modificacion ruta datos medioambientales2.0

--- controllers/datoMedioAmbientalController.js
+++ controllers/datoMedioAmbientalController.js
@@ -4,8 +4,11 @@ const DatoMedioAmbiental = require('../model/DatoMedioAmbiental')
 module.exports.crearDatoMedioAmbiental = (req, res)=>{
     console.log('POST')
     console.log(req.body)
-    const datoMedioAmbiental = new DatoMedioAmbiental();
-    
+    const datoMedioAmbiental = new DatoMedioAmbiental({
+        co2: req.body.co2,
+        humedad: req.body.humedad,
+        temperatura: req.body.temperatura,
+    });
     datoMedioAmbiental
         .save()
         .then((data) => res.json(data))
